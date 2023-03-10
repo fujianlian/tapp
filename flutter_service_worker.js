ff7eac53fb2051558184f50f0af83f5b3fbdaa8d
lianbq <lianbinqiang@hashkeytech.com>
2023-03-10 17:02:54 +0800

    Updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,9 +5,9 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "b8462dff0a43511ab23608304f04e07a",
 "favicon.ico": "801939791d62ba8cb0e3167c0e743ef0",
-"index.html": "32944aa7a1c715ca791daacf76dcadd0",
-"/": "32944aa7a1c715ca791daacf76dcadd0",
-"js/zxing.js": "8b5d2211ee7aa6fb56765efc62b8e979",
+"index.html": "0d5db1cb5068cbb952145141405c0603",
+"/": "0d5db1cb5068cbb952145141405c0603",
+"js/zxing.js": "1259d50407597e5cd08b72255e33b062",
 "js/punycode.es6.js": "f6d4ef13e67d34babf3d2efe9fb41bde",
 "js/firebasejs/9.15.0/firebase-remote-config.js": "2a567808264cc03c7a3b3c1762488f54",
 "js/firebasejs/9.15.0/firebase-app.js": "38554d47ab35af3c2ae87149519d0229",
